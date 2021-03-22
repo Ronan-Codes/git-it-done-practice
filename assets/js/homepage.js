b301684c57635547c40608eae85c69ef6376fafd
Ronan <galv.ronan@gmail.com>
2021-03-22 17:46:23 -0400

    debugged alert(error)

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -17,7 +17,7 @@ var getUserRepos = function(user) {
                     displayRepos(data, user)
                 });
             } else {
-                alert("Error: " + response.statusText);
+                alert("Error: " + response.status);
             }
         })
         .catch(function(error) {
